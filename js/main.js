53b1dd36654c09a456b53ae82e48b3026e85840c
GrPaladino <paladino.graziano@gmail.com>
2024-01-31 15:04:28 +0100

    Feat: created html structure and array in js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,53 @@ const { createApp } = Vue;
 const app = createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      title: "Todo App",
+
+      tasks: [
+        {
+          name: "Task 1",
+          type: true,
+        },
+
+        {
+          name: "Task 2",
+          type: true,
+        },
+        {
+          name: "Task 3",
+          type: false,
+        },
+
+        {
+          name: "Task 4",
+          type: false,
+        },
+
+        {
+          name: "Task 5",
+          type: true,
+        },
+
+        {
+          name: "Task 6",
+          type: false,
+        },
+
+        {
+          name: "Task 7",
+          type: true,
+        },
+
+        {
+          name: "Task 8",
+          type: true,
+        },
+
+        {
+          name: "Task 9",
+          type: false,
+        },
+      ],
     };
   },
 
